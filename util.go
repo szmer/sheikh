237ef6f64752511c7e3d3adde5991395b0113497
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2016-08-07 18:45:50 +0200

    removed some old trash from util.go

--- util.go
+++ util.go
@@ -2,58 +2,8 @@ package sheikh
 
 import "encoding/json"
 
+// Convert a thing to OrientDB-syntax string representation.
 func toOdbRepr(thing interface{}) string {
 	ret, _ := json.Marshal(thing)
 	return string(ret)
-}
-
-type relSliceAggregate struct {
-	masterIndex   *map[string][]vtxRel
-	currentSlice  *[]vtxRel
-	keys          []string
-	i             int
-	currentMapKey string
-}
-
-func newRelSliceAggregate(currentSlice []vtxRel, masterIndex map[string][]vtxRel) (rsa relSliceAggregate) {
-	if currentSlice != nil {
-		rsa.currentSlice = &currentSlice
-	} else {
-		*rsa.currentSlice = nil
-	}
-	rsa.masterIndex = &masterIndex
-	if masterIndex != nil {
-		for key, _ := range masterIndex {
-			rsa.keys = append(rsa.keys, key)
-		}
-	}
-	return
-}
-
-func (rsa *relSliceAggregate) yield() (rel vtxRel) {
-	if rsa.i < len(*rsa.currentSlice) {
-		rsa.i++
-		return (*rsa.currentSlice)[rsa.i-1]
-	}
-	if rsa.masterIndex == nil {
-		return
-	}
-	var loadNextIndex bool
-	if rsa.currentMapKey == "" { // first time
-		loadNextIndex = true
-	} else {
-		loadNextIndex = false
-	}
-	for _, key := range rsa.keys {
-		slice := (*rsa.masterIndex)[key]
-		if loadNextIndex {
-			rsa.currentSlice = &slice
-			rsa.currentMapKey = key
-			return rsa.yield()
-		}
-		if key == rsa.currentMapKey {
-			loadNextIndex = true
-		}
-	}
-	return
 }
